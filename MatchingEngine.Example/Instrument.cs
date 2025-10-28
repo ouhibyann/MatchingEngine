3ceb4cab5a461892327410ae4a3b978f95a2a8bb
youhib <yann.ouh300@gmail.com>
2025-10-28 16:02:18 +0100

    Created Example.csproj to simulate producer behaviour

---
+++ MatchingEngine.Example/Instrument.cs
@@ -0,0 +1,14 @@
+namespace MatchingEngine.Example;
+
+sealed record Instrument(
+    Guid Id,
+    decimal Price,
+    int Quantity,
+    DateTime CreatedOn
+) : IInstrument
+{
+    public static Instrument New(decimal price, int qty)
+    {
+        return new Instrument(Guid.NewGuid(), price, qty, DateTime.UtcNow);
+    }
+}
---
+++ MatchingEngine.Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Threading.Channels;
+using MatchingEngine.Example;
+using MatchingEngine.Transport; 
+
+var cts = new CancellationTokenSource();
+var hub = new Hub<Instrument>(capacity: 128, fullMode: BoundedChannelFullMode.Wait, singleWriter: false, singleReader: false);
+var producer = new Producer<Instrument>(hub);
+var consumer = new Consumer<Instrument>(hub);
+
+Stopwatch sw = Stopwatch.StartNew();
+sw.Start();
+
+var consumerTask = Task.Run(() => consumer.RunAsync(cts.Token));
+
+
+for (int i = 1; i < 1000; i++)
+{
+    var order = Instrument.New(price: 10 + i, qty: (1 + i) % i);
+    await producer.PublishAsync(order, cts.Token);
+}
+
+hub.Writer.TryComplete();
+cts.Cancel();
+sw.Stop();
+
+try { await consumerTask; }
+catch (OperationCanceledException) { Console.WriteLine($"Example Canceled. Elapsed: {sw.Elapsed}"); }
+
+Console.WriteLine($"Example finished. Elapsed: {sw.Elapsed}");
