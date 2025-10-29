7e29b67d57baceee01c9e3a022c72b50f0d694f8
youhib <yann.ouh300@gmail.com>
2025-10-29 23:57:38 +0100

    Commit this atrocity to try loggers inside the Publisher/Consumer to reduce overhead memory in .Example.csproj - there is now noway to know which publisher/consumer is logging a line

--- MatchingEngine.Example/AsyncLogger.cs
+++ MatchingEngine.Example/AsyncLogger.cs
@@ -1,43 +1,43 @@
-using System.Threading.Channels;
-
-namespace MatchingEngine.Example;
-
-public sealed class AsyncLogger
-{
-    private readonly Channel<string> _channel = Channel.CreateUnbounded<string>();
-    private readonly CancellationToken _outer;
-    private Task? _task;
-
-    public AsyncLogger(CancellationToken outer)
-    {
-        _outer = outer;
-    }
-
-    public ValueTask WriteLineAsync(string line, CancellationToken ct = default)
-    {
-        return _channel.Writer.WriteAsync(line, ct);
-    }
-
-    public Task StartAsync()
-    {
-        _task = Task.Run(async () =>
-        {
-            try
-            {
-                await foreach (var line in _channel.Reader.ReadAllAsync(_outer))
-                    Console.WriteLine(line);
-            }
-            catch (OperationCanceledException)
-            {
-                Console.WriteLine("Operation cancelled");
-            }
-        }, _outer);
-        return Task.CompletedTask;
-    }
-
-    public async Task StopAsync()
-    {
-        _channel.Writer.TryComplete();
-        if (_task is not null) await _task;
-    }
-}
+// using System.Threading.Channels;
+//
+// namespace MatchingEngine.Example;
+//
+// public sealed class AsyncLogger
+// {
+//     private readonly Channel<string> _channel = Channel.CreateUnbounded<string>();
+//     private readonly CancellationToken _outer;
+//     private Task? _task;
+//
+//     public AsyncLogger(CancellationToken outer)
+//     {
+//         _outer = outer;
+//     }
+//
+//     public ValueTask WriteLineAsync(string line, CancellationToken ct = default)
+//     {
+//         return _channel.Writer.WriteAsync(line, ct);
+//     }
+//
+//     public Task StartAsync()
+//     {
+//         _task = Task.Run(async () =>
+//         {
+//             try
+//             {
+//                 await foreach (var line in _channel.Reader.ReadAllAsync(_outer))
+//                     Console.WriteLine(line);
+//             }
+//             catch (OperationCanceledException)
+//             {
+//                 Console.WriteLine("Operation cancelled");
+//             }
+//         }, _outer);
+//         return Task.CompletedTask;
+//     }
+//
+//     public async Task StopAsync()
+//     {
+//         _channel.Writer.TryComplete();
+//         if (_task is not null) await _task;
+//     }
+// }
